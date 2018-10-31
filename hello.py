6b973ee102cd54c75eb1e68f2bbe599ab33c6f0e
Juuso-Miikka Heikkilä <juusomih@c305.(none)>
2018-10-31 12:54:10 +0200

    #3 Unfucking helloworld

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
-Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print("Hello")
-Hello
->>> 
+print("Hello")
+
+
