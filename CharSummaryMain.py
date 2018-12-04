0b28579ba834a179b90bb8814fbc85c0f3162ac0
Juuso-Miikka Heikkilä <juusomih@c307.(none)>
2018-12-05 01:35:56 +0200

    #15 Formates the actual text so thereferences part is cut out. Last actual part should always be Notes.

--- CharSummaryMain.py
+++ CharSummaryMain.py
@@ -8,6 +8,11 @@ def split_text(fulltext):
     splittext = re.split (r"}}\s*\w*'''",fulltext)
     return splittext
 
+def cut_out_extra(text_text):
+    goodtext = re.split(r"=+.*References.*=+",text_text)
+    return goodtext[0]
+    
+
 
 def query():
     query = input("Give query ")
@@ -26,17 +31,21 @@ def summary(fulltext):
 #returns the actual text part
 def text_text(fulltext):
     text_text = split_text(fulltext)
-    return text_text[1]
+    return cut_out_extra(text_text[1])
 
 def main():
     ##Crashes as a end since the connection is open try to figure out how to close properly
     ##Might not be necessary in flask/html version
     while(ender):
         text = query()
+        #the summarybox part
         summa = summary(text)
+        #the formated text part
+        actualtext = text_text(text)
 
-        #the summary is in 0 location
-        print(summa)
+        #HERE IF NEED TO SEE WHAT TEXT LOOKS LIKE
+        #print(summa)
+        #print(actualtext)
         
     
 
